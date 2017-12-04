72ae451edfe95da0086cfa9d504dc8ff2f731669
caraxiong <571982634@qq.com>
2017-12-04 23:18:35 +0800

    add .

--- static/src/components/egs/tabs/TabNav.js
+++ static/src/components/egs/tabs/TabNav.js
@@ -21,7 +21,59 @@ class TabNav extends Component {
 			}
 
 			const order = parseInt(child.props.order, 10);
+			//利用class控制显示和隐藏
+			let classes = classnames({
+				[`${classPrefix}-tab`]: true,
+				[`${classPrefix}-active`]: activeIndex === order,
+				[`${classPrefix}-disabled`]: child.props.disabled,
+			});
 
+			let events = {};
+			if (!child.props.disabled) {
+				events = {
+					onClick: this.props.onTabClick.bind(this, order),
+				}
+			}
+
+			const ref = {};
+			if (activeIndex === order) {
+				ref.ref = 'activeTab';
+			}
+
+			return (
+				<li 
+					role="tab"
+					aria-disabled = {child.props.disabled ? 'true' : 'false'}
+					aria-selected = {activeIndex === order ? 'true' : 'false'}
+					{...events}
+					className= {classes}
+					key={order}
+					{...ref}
+				>
+					{child.props.tab}
+				</li>
+			)
 		})
+	}
+	render() {
+		const {
+			classPrefix
+		} = this.props;
+
+		const rootClasses = classnames({
+			[`${classPrefix}-bar`]: true,
+		});
+
+		const classes = classnames({
+			[`${classPrefix}-nav`]: true,
+		});
+
+		return (
+			<div className={rootClasses} role="tablist">
+				<ul className={classes}>
+					{this.getTabs()}
+				</ul>
+			</div>
+		);
 	}
 }
---
+++ static/src/components/egs/tabs/TabPane.js
@@ -0,0 +1,40 @@
+import React, {
+	Component
+} from 'react'
+import PropTypes from 'prop-types'
+
+class TabPane extends Component {
+	static propTypes = {
+		tab: PropTypes.oneOfType([
+			PropTypes.string,
+			PropTypes.node,
+		]).isRequired,
+		order: PropTypes.string.isRequired,
+		disabled: PropTypes.bool,
+		isActive: PropTypes.bool,
+	};
+	render() {
+		const {
+			classPrefix,
+			className,
+			isActive,
+			children
+		} = this.props
+
+		const classes = classnames({
+			[className]: className,
+			[`${classPrefix}-panel`]: true,
+			[`${classPrefix}-active`]: isActive,
+		});
+
+		return (
+			<div 
+				role="tabpanel"
+				className={classes}
+				aria-hidden={!isActive}
+			>
+				{children}
+			</div>
+		)
+	}
+}
